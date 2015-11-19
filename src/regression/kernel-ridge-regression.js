5e0f6c52f456095b485338a53095b3dbb233d304
Michaël Zasso <mic.besace@gmail.com>
2015-11-19 08:26:04 +0100

    refactor KRR to a class

---
+++ src/regression/kernel-ridge-regression.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Matrix = require('ml-matrix');
+const Kernel = require('ml-kernel');
+
+const defaultOptions = {
+    lambda: 0.1,
+    kernelType: 'gaussian',
+    kernelOptions: {}
+};
+
+// Implements the Kernel ridge regression algorithm.
+// http://www.ics.uci.edu/~welling/classnotes/papers_class/Kernel-Ridge.pdf
+class KernelRidgeRegression {
+    constructor(inputs, outputs, options) {
+        if (inputs === true) { // reloading model
+            this.alpha = outputs.alpha;
+            this.inputs = outputs.inputs;
+            this.kernelType = outputs.kernelType;
+            this.kernelOptions = outputs.kernelOptions;
+            this.kernel = new Kernel(outputs.kernelType, outputs.kernelOptions);
+        } else {
+            options = Object.assign({}, defaultOptions, options);
+
+            const kernelFunction = new Kernel(options.kernelType, options.kernelOptions);
+            const K = kernelFunction.compute(inputs);
+            const n = inputs.length;
+            K.add(Matrix.eye(n, n).mul(options.lambda));
+
+            this.alpha = K.solve(outputs);
+            this.inputs = inputs;
+            this.kernelType = options.kernelType;
+            this.kernelOptions = options.kernelOptions;
+            this.kernel = kernelFunction;
+        }
+    }
+
+    predict(newInputs) {
+        return this.kernel.compute(newInputs).mmul(this.alpha);
+    }
+
+    toJSON() {
+        return {
+            name: 'kernelRidgeRegression',
+            alpha: this.alpha,
+            inputs: this.inputs,
+            kernelType: this.kernelType,
+            kernelOptions: this.kernelOptions
+        };
+    }
+
+    static load(json) {
+        if (json.name !== 'kernelRidgeRegression') {
+            throw new TypeError('not a KRR model');
+        }
+        return new KernelRidgeRegression(true, json);
+    }
+}
+
+module.exports = KernelRidgeRegression;
---
+++ test/regression/kernel-ridge-regression.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var Matrix = require('ml-matrix');
+var KernelRidgeRegression = require('../..').KernelRidgeRegression;
+
+var nSamples = 10;
+var nVars = 2;
+
+var Xs = Matrix.random(nSamples, nVars);
+Xs.sub(0.5);
+var Ys = Matrix.zeros(nSamples, 1);
+for (var i = 0; i < nSamples; i++) {
+    Ys[i][0] = Xs[i][0] * Xs[i][0] + 2 * Xs[i][0] * Xs[i][1] + Xs[i][1] * Xs[i][1];
+}
+
+describe('Kernel ridge regression', function () {
+    it('Polynomial kernel should overfit the pattern', function () {
+        var model = new KernelRidgeRegression(Xs, Ys, {
+            kernelType: 'polynomial',
+            lambda: 0.0001,
+            kernelParams: {degree: 2, bias: 1}
+        });
+        var Y = model.predict(Xs);
+
+        for (i = 0; i < Y.length; i++) {
+            Y[i][0].should.be.approximately(Ys[i][0], 5e-3);
+        }
+    });
+    it('Gaussian kernel should overfit the pattern', function () {
+        var model = new KernelRidgeRegression(Xs, Ys, {
+            kernelType: 'gaussian',
+            lambda: 0.0001,
+            kernelOptions: {sigma: 0.1}
+        });
+        var Y = model.predict(Xs);
+        for (var i = 0; i < Y.length; i++) {
+            Y[i][0].should.be.approximately(Ys[i][0], 5e-3);
+        }
+    });
+});
