0f7760748d53e74fd8a006ccb9e5279186f45af3
maasencioh <maasencioh@gmail.com>
2016-09-20 18:05:53 +0200

    feat(theil-sen-regression): add Theil-Sen regression and his test

--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ exports.KernelRidgeRegression = exports.KRR = require('./regression/kernel-ridge
 //exports.MultipleLinearRegression = exports.MLR = require('./regression/multiple-linear-regression');
 //exports.MultivariateLinearRegression = exports.MVLR = require('./regression/multivariate-linear-regression');
 exports.PolinomialFitting2D = require('./regression/poly-fit-regression2d');
+exports.TheilSenRegression = require('./regression/theil-sen-regression');
---
+++ src/regression/theil-sen-regression.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const BaseRegression = require('./base-regression');
+const maybeToPrecision = require('./util').maybeToPrecision;
+
+/**
+ * Theil–Sen estimator
+ *
+ * https://en.wikipedia.org/wiki/Theil%E2%80%93Sen_estimator
+ * @class
+ */
+class TheilSenRegression extends BaseRegression {
+
+    /**
+     *
+     * @param x
+     * @param y
+     * @param options
+     * @constructor
+     */
+    constructor(x, y, options) {
+        options = options || {};
+        super();
+        if (x === true) {
+            // loads the model
+            this.slope = y.slope;
+            this.intercept = y.intercept;
+            this.quality = y.quality || {};
+            if (y.quality.r) {
+                this.quality.r = y.quality.r;
+                this.quality.r2 = y.quality.r2;
+            }
+            if (y.quality.chi2) {
+                this.quality.chi2 = y.quality.chi2;
+            }
+        } else {
+            // creates the model
+            let len = x.length;
+            if (len !== y.length) {
+                throw new RangeError('Input and output array have a different length');
+            }
+
+            let slopes = new Array(len * len);
+            let count = 0;
+            for (let i = 0; i < len; ++i) {
+                for (let j =  i + 1; j < len; ++j) {
+                    if (x[i] !== x[j]) {
+                        slopes[count++] = (y[j] - y[i]) / (x[j] - x[i]);
+                    }
+                }
+            }
+            slopes.length = count;
+            let medianSlope = slopes.sort()[Math.floor(count / 2)];
+
+            let cuts = new Array(len);
+            for (let i = 0; i < len; ++i) {
+                cuts[i] = y[i] - medianSlope * x[i];
+            }
+
+            this.slope = medianSlope;
+            this.intercept = cuts.sort()[Math.floor(len / 2)];
+            this.coefficients = [this.intercept, this.slope];
+            if (options.computeQuality) {
+                this.quality = this.modelQuality(x, y);
+            }
+        }
+
+    }
+
+    toJSON() {
+        var out = {
+            name: 'TheilSenRegression',
+            slope: this.slope,
+            intercept: this.intercept
+        };
+        if (this.quality) {
+            out.quality = this.quality;
+        }
+
+        return out;
+    }
+
+    _predict(input) {
+        return this.slope * input + this.intercept;
+    }
+
+    computeX(input) {
+        return (input - this.intercept) / this.slope;
+    }
+
+    toString(precision) {
+        var result = 'y = ';
+        if (this.slope) {
+            var xFactor = maybeToPrecision(this.slope, precision);
+            result += (Math.abs(xFactor - 1) < 1e-5 ? '' : xFactor) + 'x';
+            if (this.intercept) {
+                var absIntercept = Math.abs(this.intercept);
+                var operator = absIntercept === this.intercept ? '+' : '-';
+                result += ' ' + operator + ' ' + maybeToPrecision(absIntercept, precision);
+            }
+        } else {
+            result += maybeToPrecision(this.intercept, precision);
+        }
+        return result;
+    }
+
+    toLaTeX(precision) {
+        return this.toString(precision);
+    }
+
+    static load(json) {
+        if (json.name !== 'TheilSenRegression') {
+            throw new TypeError('not a Theil-Sen model');
+        }
+        return new TheilSenRegression(true, json);
+    }
+}
+
+module.exports = TheilSenRegression;
