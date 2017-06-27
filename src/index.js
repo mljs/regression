072df5a23400812a5c9c7ebdcac06e438731a413
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-27 11:24:48 +0200

    fix: export modules as default (#27)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-exports.SimpleLinearRegression = exports.SLR = require('ml-regression-simple-linear');
-exports.PolynomialRegression = require('ml-regression-polynomial');
-exports.ExponentialRegression = require('ml-regression-exponential');
-exports.PowerRegression = require('ml-regression-power');
+exports.SimpleLinearRegression = exports.SLR = require('ml-regression-simple-linear').default;
+exports.PolynomialRegression = require('ml-regression-polynomial').default;
+exports.ExponentialRegression = require('ml-regression-exponential').default;
+exports.PowerRegression = require('ml-regression-power').default;
 
 exports.NonLinearRegression = exports.NLR = {
     PotentialRegression: require('./regression/potential-regression')
@@ -14,5 +14,5 @@ exports.KernelRidgeRegression = exports.KRR = require('./regression/kernel-ridge
 exports.PolinomialFitting2D = require('./regression/poly-fit-regression2d');
 
 // robust regressions
-exports.TheilSenRegression = require('ml-regression-theil-sen');
-exports.RobustPolynomialRegression = require('ml-regression-robust-polynomial');
+exports.TheilSenRegression = require('ml-regression-theil-sen').default;
+exports.RobustPolynomialRegression = require('ml-regression-robust-polynomial').default;
