152478feb50671eee5738f2ff1107c95ddf1e4e8
Michaël Zasso <targos@protonmail.com>
2017-06-28 17:50:09 +0200

    feat: add MultivariateLinearRegression

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ exports.SimpleLinearRegression = exports.SLR = require('ml-regression-simple-lin
 exports.PolynomialRegression = require('ml-regression-polynomial').default;
 exports.ExponentialRegression = require('ml-regression-exponential').default;
 exports.PowerRegression = require('ml-regression-power').default;
+exports.MultivariateLinearRegression = require('ml-regression-multivariate-linear').default;
 
 exports.NonLinearRegression = exports.NLR = {
     PotentialRegression: require('./regression/potential-regression')
 };
 exports.KernelRidgeRegression = exports.KRR = require('./regression/kernel-ridge-regression');
 //exports.MultipleLinearRegression = exports.MLR = require('./regression/multiple-linear-regression');
-//exports.MultivariateLinearRegression = exports.MVLR = require('./regression/multivariate-linear-regression');
 exports.PolinomialFitting2D = require('./regression/poly-fit-regression2d');
 
 // robust regressions
